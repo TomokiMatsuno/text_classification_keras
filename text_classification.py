83f4b0657ff5cebb016fccbf75c1abda5ab59182
TomokiMatsuno <tomoki.matsuno256@gmail.com>
2018-09-06 10:41:25 +0900

    minibatch

--- text_classification.py
+++ text_classification.py
@@ -15,10 +15,25 @@ print(tf.__version__)
 # imdb = keras.datasets.imdb
 #
 # (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
-train_labels, test_labels, train_data, test_data = train_test_split(df_tweets[0].tolist(), [[int(e) for e in r.strip().split()] for r in df_tweets[1].tolist()], test_size=0.2)
+# train_labels, test_labels, train_data, test_data = train_test_split(df_tweets[0].tolist(), [[int(e) for e in r.strip().split()] for r in df_tweets[1].tolist()], test_size=0.2)
+train_labels, test_labels, train_data, test_data = train_test_split(df_tweets[0].tolist(), [[int(e) for e in r.strip().split()] if isinstance(r, str) else [0] for r in df_tweets[1].tolist()], test_size=0.2)
+
+
+train_labels_data = [(l, d) for l, d in zip(train_labels, train_data)]
+train_labels_data = sorted(train_labels_data, key=lambda x:len(x[1]))
 
 print("Training entries: {}, labels: {}".format(len(train_data), len(train_labels)))
 
+train_data_splits = []
+step_size = len(train_labels_data) // 64
+for i in range(0, len(train_labels_data), step_size):
+    train_data_splits.append(train_labels_data[i: i + step_size])
+
+
+train_labels = [[e[0] for e in s] for s in train_data_splits]
+train_data = [[e[1] for e in s]for s in train_data_splits]
+
+[(min([len(e) for e in s]), max([len(e) for e in s])) for s in train_data]
 print(train_data[0])
 
 print(len(train_data[0]), len(train_data[1]))
@@ -39,28 +54,7 @@ print(len(train_data[0]), len(train_data[1]))
 #     return ' '.join([reverse_word_index.get(i, '?') for i in text])
 #
 # decode_review(train_data[0])
-#
-# train_data = keras.preprocessing.sequence.pad_sequences(train_data,
-#                                                         value=word_index["<PAD>"],
-#                                                         padding='post',
-#                                                         maxlen=256)
-#
-# test_data = keras.preprocessing.sequence.pad_sequences(test_data,
-#                                                        value=word_index["<PAD>"],
-#                                                        padding='post',
-#                                                        maxlen=256)
 
-
-train_data = keras.preprocessing.sequence.pad_sequences(train_data,
-                                                        value=0,
-                                                        padding='post',
-                                                        maxlen=256)
-
-
-test_data = keras.preprocessing.sequence.pad_sequences(test_data,
-                                                       value=0,
-                                                       padding='post',
-                                                       maxlen=256)
 
 # train_data_fwd = keras.preprocessing.sequence.pad_sequences(train_data,
 #                                                         value=0,
@@ -104,29 +98,82 @@ model.compile(optimizer=tf.train.AdamOptimizer(),
 #               loss='binary_crossentropy',
 #               metrics=[P, R, F])
 
-x_val = train_data[:10000]
-partial_x_train = train_data[10000:]
-
-y_val = train_labels[:10000]
-partial_y_train = train_labels[10000:]
-class_weight = {0:sum(train_labels)/len(train_labels), 1:1.}
-history = model.fit(train_data,
-                    train_labels,
-                    epochs=3,
-                    batch_size=512,
-                    validation_data=(x_val, y_val),
-                    verbose=1,
-                    class_weight=class_weight)
-
-pred_labels = [int(e[0]) for e in model.predict_classes(test_data)]
 
 
-results = model.evaluate(test_data, test_labels)
+# train_data = keras.preprocessing.sequence.pad_sequences(train_data,
+#                                                         value=0,
+#                                                         padding='post',
+#                                                         maxlen=140)
 
-print(classification_report(test_labels, pred_labels))
-print(confusion_matrix(test_labels, pred_labels))
 
-print(results)
+x_val = keras.preprocessing.sequence.pad_sequences(test_data,
+                                                       value=0,
+                                                       padding='post',
+                                                       maxlen=140)
+
+
+# x_val = train_data[:2000]
+# x_val = test_data
+partial_x_train = train_data[2000:]
+
+# y_val = train_labels[:2000]
+y_val = test_labels
+partial_y_train = train_labels[2000:]
+class_weight = {0:sum(test_labels)/len(test_labels), 1:1.}
+
+partial_x_train = []
+for td in train_data:
+    partial_x_train.append(keras.preprocessing.sequence.pad_sequences(td,
+                                                            value=0,
+                                                            padding='post',
+                                                            maxlen=len(max(td,key=len))))
+
+epochs=30
+for epc in range(epochs):
+    steps = np.random.permutation([step for step in range(len(train_data_splits))])
+    for step in steps:
+        # partial_x_train = train_data[step]
+        partial_y_train = train_labels[step]
+        history = model.fit(partial_x_train[step],
+                            partial_y_train,
+                            epochs=1,
+                            batch_size=500,
+                            verbose=0,
+                            class_weight=class_weight)
+        # validation_data = (x_val, y_val),
+    if epc % 10 == 0 and epc > 0:
+        failed = 0
+        pred_scores = []
+
+        for d in test_data:
+            try:
+                pred_scores.append(model.predict(np.matrix(d if d else [0])))
+            except:
+                failed += 1
+                print(failed)
+
+        pred_labels = [int(v > 0.5) for v in pred_scores]
+        print(classification_report(test_labels, pred_labels))
+        print(confusion_matrix(test_labels, pred_labels))
+
+# pred_labels = [model.predict(np.matrix(d)) for d in test_data]
+
+#
+# failed = 0
+# pred_labels = []
+#
+# for d in test_data:
+#     try:
+#         pred_labels.append(model.predict(np.matrix(d if d else [0])))
+#     except:
+#         failed += 1
+#         print(failed)
+#
+# pred_labels = [int(v > 0.5) for v in pred_labels]
+# print(classification_report(test_labels, pred_labels))
+# print(confusion_matrix(test_labels, pred_labels))
+
+# print(results)
 
 history_dict = history.history
 print(history_dict.keys())
